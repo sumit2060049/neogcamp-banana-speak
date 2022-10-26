7f102535143e87644d894a87d2c4b5edc7634f57
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2022-10-27 02:52:34 +0530

    upto callback

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-console.log("hello!script is working form a different file")
+//  console.log("hello!script is working form a different file")
 
-var username=prompt("give me your username")
-alert("this scritpt works "+username)
+//  var username=prompt("give me your username")
+//  alert("this scritpt works "+username)
+
+var btnTranslate = document.querySelector("#btn-translate");
+//console.log(btnTranslate);
+function clickHandler(){
+    alert("hello");
+}
+ btnTranslate.addEventListener("click",/*function clickEventHandler(){
+     console.log("clicked")
+    // alert("clicked")
+}*/clickHandler)
