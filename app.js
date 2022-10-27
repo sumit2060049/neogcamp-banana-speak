dc0f9ed791b2e02631bddcfc6f7f1e7098bcef69
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2022-10-27 20:30:21 +0530

    all done

--- app.js
+++ app.js
@@ -12,11 +12,35 @@ var outputdiv=document.querySelector("#output");
 //console.log(outputdiv);
 
 //console.log(btnTranslate);
+// var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+	
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server:try again after some time")
+}
 function clickHandler(){
     //console.log("clicked");
     //console.log("input",txtInput.value);
-    outputdiv.innerText="ajsjsjsjsjjs "+txtInput.value;
-}
+   // outputdiv.innerText="ajsjsjsjsjjs "+txtInput.value;
+   var inputText=txtInput.value;
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json() )
+   .then(json => {
+    var translatedText=json.contents.translated;
+    outputdiv.innerText=translatedText;
+    
+   })
+   .catch(errorHandler)
+
+   //.then(json => console.log(json.contents.translated))
+   
+};
  btnTranslate.addEventListener("click",/*function clickEventHandler(){
      console.log("clicked")
     // alert("clicked")
